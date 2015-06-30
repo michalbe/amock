78806b0e39a23632dfc540eea366ac1d5573f8cf
Michał Budzyński <michal@virtualdesign.pl>
2015-06-30 13:45:14 +0200

    :closed_umbrella: proper helper parsing

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 module.exports = (function(){
   'use strict';
+  var helpers = require('./utils/helpers');
+
+  var specialActions = {
+    image: function(res){
+      return 'http://lorempixel.com/' + res + '?' + ~~(Math.random()*999999);
+    },
+    id: function(){
+      return id++;
+    }
+  };
 
   var mocks = {};
+  var id = 0;
   var init = function(name, schema){
     mocks[name] = schema;
   };
 
   var parseElement = function(element) {
-    console.log('parsing', element);
     var args = element.split(':');
     var name = args[0];
     args = args[1];
-
-    console.log(name, args);
+    
+    if (name in specialActions) {
+      return specialActions[name](args);
+    } else if (name in helpers){
+      return helpers[name](args);
+    } else {
+      return name;
+    }
   };
 
   init.get = function(){
@@ -30,6 +46,6 @@ var e = {
 };
 
 for (var key in e) {
-  parseElement(e[key]);
+  console.log(key, ': ', parseElement(e[key]));
 }
 })();
