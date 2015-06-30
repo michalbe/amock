8ba7c26346c7acc70b03f72c7676ac66527974cc
Michał Budzyński <michal@virtualdesign.pl>
2015-06-30 13:24:02 +0200

    :ocean: arguments parsing

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ module.exports = (function(){
     mocks[name] = schema;
   };
 
+  var parseElement = function(element) {
+    console.log('parsing', element);
+    var args = element.split(':');
+    var name = args[0];
+    args = args[1];
+
+    console.log(name, args);
+  };
 
   init.get = function(){
     return 'hej';
   };
 
-  return init;
+//  return init;
+
+var e = {
+  id: 'id',
+  age: 'random-number:10-20',
+  login: 'words:1',
+  name: 'names:2',
+  description: 'sentences:5'
+};
+
+for (var key in e) {
+  parseElement(e[key]);
+}
 })();
